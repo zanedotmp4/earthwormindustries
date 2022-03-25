d02995a95739ee5663694d2d49e29a5835660129
BreretonCodez <53999272+BreretonCodez@users.noreply.github.com>
2022-03-25 01:23:27 +0000

    Corrected spelling

--- App/models/listing.py
+++ App/models/listing.py
@@ -1,6 +1,6 @@
 from App.database import db
 
-listing = db.Table('listing',                db.Column('listingID',db.Interger,primary_key=True),
+listing = db.Table('listing',                db.Column('listingID',db.Integer,primary_key=True),
 db.Column('cropId', db.Integer, db.ForeignKey('crop.id')),
-db.Column('livestockID',db.Interger,db.ForeignKey('livestock.id'))
+db.Column('livestockID',db.Integer,db.ForeignKey('livestock.id'))
 )
