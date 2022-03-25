7b5dd6f48173d6ef970beef5107b0bfa1e96c96c
BreretonCodez <53999272+BreretonCodez@users.noreply.github.com>
2022-03-25 01:18:17 +0000

    Removed comma

--- App/models/listing.py
+++ App/models/listing.py
@@ -2,5 +2,5 @@ from App.database import db
 
 listing =db.Table('listing',                db.Column('listingID',db.Interger,primary_key=True),
 db.Column('cropId', db.Integer, db.ForeignKey('crop.id')),
-db.Column('livestockID',db.Interger,db.ForeignKey('livestock.id')),
+db.Column('livestockID',db.Interger,db.ForeignKey('livestock.id'))
 )
