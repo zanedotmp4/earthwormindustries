761a43670f3b1e3ababc33b39c3716c801a47121
zanedotmp4 <zaneamour@gmail.com>
2022-04-24 03:22:42 +0000

    updated user

--- App/models/user.py
+++ App/models/user.py
@@ -1,5 +1,8 @@
 from werkzeug.security import check_password_hash, generate_password_hash
 from App.database import db
+from .crops import *
+from .chemicals import *
+from .livestock import *
 # from App.models import listing
 from flask_login import UserMixin
 
@@ -16,7 +19,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username =  db.Column(db.String, nullable=False)
     password = db.Column(db.String(120), nullable=False)
-    # listings = db.relationship('Listing', secondary=listings, lazy='subquery', backref=db.backref('users', lazy=True))
+    listings = db.relationship('Listing', secondary=listings, lazy='subquery', backref=db.backref('users', lazy=True))
     def __init__(self, username, password):
         self.username = username
         self.set_password(password)
