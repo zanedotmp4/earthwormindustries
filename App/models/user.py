26c28d9b325f754a7165f23fef63f27155478b4b
BreretonCodez <micahbrereton@hotmail.com>
2022-05-16 14:01:18 +0000

    Update user model.

--- App/models/user.py
+++ App/models/user.py
@@ -3,23 +3,20 @@ from App.database import db
 from .crops import *
 from .chemicals import *
 from .livestock import *
-# from App.models import listing
 from flask_login import UserMixin
 
-class Listing(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-
-listings = db.Table('listings',
-    db.Column('crop_id', db.Integer, db.ForeignKey('crop.id'), primary_key=True),
-    db.Column('livestock_id', db.Integer, db.ForeignKey('livestock.id'), primary_key=True),
-    db.Column('chemical_id', db.Integer, db.ForeignKey('chemical.id'), primary_key=True)
-)
+#listings = db.Table('listings',
+    #db.Column('crop_id', db.Integer, db.ForeignKey('crop.id'), primary_key=True),
+    #db.Column('livestock_id', db.Integer, db.ForeignKey('livestock.id'), primary_key=True),
+    #db.Column('chemical_id', db.Integer, db.ForeignKey('chemical.id'), primary_key=True)
+#)
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username =  db.Column(db.String, nullable=False)
     password = db.Column(db.String(120), nullable=False)
-    listings = db.relationship('Listing', secondary=listings, lazy='subquery', backref=db.backref('users', lazy=True))
+    listings = db.relationship('Listing', backref='user', lazy=True)
+
     def __init__(self, username, password):
         self.username = username
         self.set_password(password)
@@ -38,3 +35,6 @@ class User(db.Model, UserMixin):
         """Check hashed password."""
         return check_password_hash(self.password, password)
 
+class Listing(db.Model):
+    list_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
